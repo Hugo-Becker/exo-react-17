0e4f600849a1fa8e33aab4c384c1a87d9805c8e9
Hugo <becker.hugo@hotmail.com>
2021-01-20 09:53:45 +0100

    s1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+
+  let [count, setCount] = useState(0);
+
+  let addOne=()=>{
+    setCount(count+1)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Le compte est de {count}</div>
+      {/* <button onClick={()=>setCount(count+1)}>Plus un </button> */}
+      <button onClick={addOne}> Plus un </button>
+
+
+      
     </div>
   );
 }
